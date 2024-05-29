0762f1010a72dba7de968a91c88115692d573e80
Taro Serigano <taroserigano@gmail.com>
2024-05-28 21:19:42 -0500

    dependencies changed for useEffect

--- app/ai-suggestion/page.tsx
+++ app/ai-suggestion/page.tsx
@@ -2,17 +2,17 @@
 
 // ai-suggestion/page.tsx
 
-// Purpose: ChatGPT will generate the recipe for the selected food menu 
-// Options: language, word limit, 
+// Purpose: ChatGPT will generate the recipe for the selected food menu
+// Options: language, word limit,
 
-import { useState, useEffect, Suspense } from 'react';
-import { usePathname, useRouter, useSearchParams } from 'next/navigation';
-import { generateChatResponse } from '@/utils/actions';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css'; 
+import { useState, useEffect, Suspense } from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { generateChatResponse } from "@/utils/actions";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const AiSuggestion = () => {
-  // grab the search params 
+  // grab the search params
   const searchParams = useSearchParams();
   const router = useRouter();
   let recipe = searchParams.get("recipe");
@@ -24,9 +24,9 @@ const AiSuggestion = () => {
   const [suggestions, setSuggestions] = useState<string[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
-  const [data, setData] = useState('');
+  const [data, setData] = useState("");
   const [wordLimit, setWordLimit] = useState<number>(100);
-  const [language, setLanguage] = useState<string>('en'); // default to English
+  const [language, setLanguage] = useState<string>("en"); // default to English
 
   const searchRecipe = async (recipe: any, limit: number, lang: string) => {
     setLoading(true);
@@ -35,35 +35,36 @@ const AiSuggestion = () => {
     try {
       const response = await generateChatResponse(recipe, limit, lang);
       setData(response ?? "");
-      setSuggestions(response ? response.split('\n') : []);
+      setSuggestions(response ? response.split("\n") : []);
       setLoading(false);
     } catch (error) {
-      setError('Error fetching AI suggestions');
+      setError("Error fetching AI suggestions");
       setLoading(false);
     }
   };
 
-  // if condition changed, do the search again 
+  // if condition changed, do the search again
   useEffect(() => {
-
-    // if already result was generated, skip the repeated search again 
+    // if already result was generated, skip the repeated search again
     if (suggestions.length === 0 || suggestions === null) {
       searchRecipe(recipe, wordLimit, language);
     } else {
       setLoading(false);
     }
-  }, [recipe, suggestions, wordLimit, language]);
+  }, [recipe, suggestions]);
 
-  // save the generated recipe, add to the selected food 
+  // save the generated recipe, add to the selected food
   const saveRecipe = () => {
-    if (typeof window !== 'undefined') {
-      // load up from local storage 
-      const recipeData = JSON.parse(localStorage.getItem('data') || '[]');
-      // find the corresponding menu and add that recipe to the menu 
+    if (typeof window !== "undefined") {
+      // load up from local storage
+      const recipeData = JSON.parse(localStorage.getItem("data") || "[]");
+      // find the corresponding menu and add that recipe to the menu
       const updatedrecipeData = recipeData.map((recipeData: any) =>
-        recipeData.content === recipe ? { ...recipeData, recipe: data } : recipeData
+        recipeData.content === recipe
+          ? { ...recipeData, recipe: data }
+          : recipeData
       );
-      localStorage.setItem('data', JSON.stringify(updatedrecipeData));
+      localStorage.setItem("data", JSON.stringify(updatedrecipeData));
       toast.success("Recipe saved to recipeData!", { autoClose: 500 });
     }
   };
@@ -71,19 +72,25 @@ const AiSuggestion = () => {
   return (
     <div className="container mx-auto p-4 dark:bg-gray-900 dark:text-white transition duration-300">
       <ToastContainer />
-      <h1 className="text-3xl font-bold mb-6 text-center">Recipe Screen for {recipe} 😉</h1>
+      <h1 className="text-3xl font-bold mb-6 text-center">
+        Recipe Screen for {recipe} 😉
+      </h1>
 
       <div className="text-center mb-6">
-        <label htmlFor="wordLimit" className="mr-2">Word Limit:</label>
+        <label htmlFor="wordLimit" className="mr-2">
+          Word Limit:
+        </label>
         <input
           id="wordLimit"
           type="number"
           value={wordLimit}
-          onChange={(e) => setWordLimit(parseInt(e.target.value))} // set word limit 
+          onChange={(e) => setWordLimit(parseInt(e.target.value))} // set word limit
           className="w-20 p-2 border rounded dark:bg-gray-800 dark:text-white"
         />
 
-        <label htmlFor="language" className="mr-2 ml-4">Language:</label>
+        <label htmlFor="language" className="mr-2 ml-4">
+          Language:
+        </label>
         <select
           id="language"
           value={language}
